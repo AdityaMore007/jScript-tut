15c91e2742f93c50fd1e78cbfbdec8f3aab0bcff
Aditya More <144462086+AdityaMore007@users.noreply.github.com>
2024-10-03 20:29:26 +0000

    converion of datatype

--- 01_basics/03_dataconversion.js
+++ 01_basics/03_dataconversion.js
@@ -10,4 +10,15 @@ let valueInNumber = Number(score)
 console.log(typeof valueInNumber);
 console.log(valueInNumber);// here the value comes out to be NaN[Not a Number]
 
+//Notes:
+//"33" => 33
+//"33abc" => NaN
+// true => 1; false => 0
+//"" => false
+//"ADITYA" => true
+
+let isLoggedIn = "Aditya"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
 

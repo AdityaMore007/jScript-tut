180136fec41b675a884290a0df101777735983e2
Aditya More <144462086+AdityaMore007@users.noreply.github.com>
2024-10-03 18:49:51 +0000

    done

---
+++ 01_basics/03_dataconversion.js
@@ -0,0 +1,13 @@
+let score = "33abc"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+//many of the datatype are started in capital letter 
+
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);// here the value comes out to be NaN[Not a Number]
+
+

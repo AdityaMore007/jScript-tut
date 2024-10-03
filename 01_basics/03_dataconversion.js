c1d27bafb5e6e7ffae9f4351ef304e5c4c0ec626
Aditya More <144462086+AdityaMore007@users.noreply.github.com>
2024-10-03 20:37:31 +0000

    some changes

--- 01_basics/03_dataconversion.js
+++ 01_basics/03_dataconversion.js
@@ -1,24 +1,32 @@
-let score = "33abc"
+// let score = "33abc"
 
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
 
-//many of the datatype are started in capital letter 
+// //many of the datatype are started in capital letter 
 
 
-let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);// here the value comes out to be NaN[Not a Number]
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);// here the value comes out to be NaN[Not a Number]
 
-//Notes:
-//"33" => 33
-//"33abc" => NaN
-// true => 1; false => 0
-//"" => false
-//"ADITYA" => true
+// //Notes:
+// //"33" => 33
+// //"33abc" => NaN
+// // true => 1; false => 0
+// //"" => false
+// //"ADITYA" => true
 
-let isLoggedIn = "Aditya"
+// let isLoggedIn = "Aditya"
 
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
 
+
+//-------------------operation-----------------
+
+let value = 3
+let negvalue = -value
+console.log(negvalue);
+
+ 

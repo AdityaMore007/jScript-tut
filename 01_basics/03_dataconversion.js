bff75e8d05dac6b6564c1612afd2580deb8c55f2
Aditya More <144462086+AdityaMore007@users.noreply.github.com>
2024-10-05 18:43:17 +0000

    COMMIT1

--- 01_basics/03_dataconversion.js
+++ 01_basics/03_dataconversion.js
@@ -27,6 +27,42 @@
 
 let value = 3
 let negvalue = -value
-console.log(negvalue);
+console.log(negvalue); 
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+console.log(+true);
+console.log(+"");
+
+let gameCounter = 100;
+++gameCounter;
+console.log(gameCounter);
+
+//INCREMENT:
+//1.If used postfix, with operator after operand (for example, x++),
+//  the increment operator increments and returns the value before 
+// incrementing.
+
+//2. If used prefix, with operator before operand (for example, ++x)
+// , the increment operator increments and returns the value after
+//  incrementing.
+
+let x = 3
+let y = ++x
+
+
+
+console.log(`x:${x},y:${y}`);
+
+
+
+
+
+
+
 
  

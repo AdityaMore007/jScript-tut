7ec391e0a6754200af8118a08982527f602bc31c
Aditya More <144462086+AdityaMore007@users.noreply.github.com>
2024-10-26 07:19:55 +0000

    changes

---
+++ 01_basics/strings.js
@@ -0,0 +1,45 @@
+const name = "hitesh"
+const repoCount = 50
+
+// console.log(name + repoCount + " Value");
+// console.log(`Hello my name is ${name} and my Repo count is ${repoCount}`);
+
+const gameName = new String('hitesh-hc-com')
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.charAt(2));
+// console.log(gameName.toUpperCase());
+// // console.log(gameName.indexOf('t'));
+
+// const newString = gameName.substring(0,4)
+// console.log(newString);
+
+// const anotherString = gameName.slice(-6,4)
+// console.log(anotherString)
+
+// const newStringOne = "   hitesh   "
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+
+// const sdt = "CAT".charAt(0);
+// console.log(sdt);
+
+// str1 = "Aditya"
+// str2 = "Arahdya"
+
+// function areEqualCaseSensitive(str1, str2){
+//     return str1 === str2;
+// }
+
+// areEqualCaseSensitive();
+
+const url = "https://hitesh.com/hitesh%20choudhary"
+
+console.log(url.replace('%20','-'));
+console.log(url.includes('hitesh'));
+// console.log(url.)
+
+console.log(gameName.split("-"));

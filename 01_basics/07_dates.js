ef1a11fb4190c33e7b54f445921280c3246fe95a
Aditya More <144462086+AdityaMore007@users.noreply.github.com>
2024-10-28 18:17:48 +0000

    done for today

---
+++ 01_basics/07_dates.js
@@ -0,0 +1,39 @@
+// Dates are of the type OBJECT
+
+
+let myDate = new Date() 
+
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toLocaleTimeString());
+// console.log(myDate.toTimeString());
+// console.log(myDate.toDateString());
+
+// // console.log(typeof myDate);
+
+// let myCreatedDate = new Date(2024,0,23,4,9)
+// console.log(myCreatedDate.toString());
+// // console.log(myCreatedDate.toDateString());
+// console.log(myCreatedDate.toLocaleString());
+
+let date1 = new Date("01-25-2023")
+// console.log(date1.toLocaleDateString());
+
+
+let myTimeStamp = Date.now()
+// console.log(myTimeStamp);
+// console.log(date1.getTime());
+console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getDay());
+
+
+
+
+
+

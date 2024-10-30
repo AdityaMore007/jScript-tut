31c0005c52bab1aa8286f935c21e46b72ed75e19
Aditya More <144462086+AdityaMore007@users.noreply.github.com>
2024-10-30 08:09:15 +0000

    array done for today

--- 02_basics/01_array.js
+++ 02_basics/01_array.js
@@ -1,4 +1,10 @@
 // Array
 
 const myArr = [0,1,2,3,4,5]
- 
+
+//1. JavaScript ARRAY are resizable.
+//2. JavaScript array are not associative array.
+// for accessing elements we have to access it through indexing.
+//3. **JavaScript array-copy oprations create shallow copies and deep copies**.
+
+const myArr2 = new Array(1,2,3,4)

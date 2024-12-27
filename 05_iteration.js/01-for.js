13f8a877a398008a20cef0984f18eee9c7c00c5b
Aditya More <144462086+AdityaMore007@users.noreply.github.com>
2024-12-27 10:34:36 +0000

    done

--- 05_iteration.js/01-for.js
+++ 05_iteration.js/01-for.js
@@ -21,12 +21,52 @@ for (let i = 1; i <= 10; i++) {
   }
 }
 
-let myArray = ["flash","batman","superman"]
-console.log(myArray.length);
+// let myArray = ["flash","batman","superman"]
+// console.log(myArray.length);
 
-for (let index = 0; index <= myArray.length; index++) {
-  const element = myArray[index];
-  console.log(element);
+// for (let index = 0; index <= myArray.length; index++) {
+//   const element = myArray[index];
+  // console.log(element);
+  
+// }
+
+// Break and continue
+
+// for (let index = 1; index <= 20; index++) {
+//   if (index == 5){
+//     console.log(`Detected 5`);
+//     break
+    
+//   }
+//   console.log(`Value of i is ${index}`);
+// }
+
+
+
+for (let index = 1; index <= 20; index++) {
+  if (index == 5){
+    // console.log(`Detected 5`);
+    continue
+  }
+  // console.log(`Value of i is ${index}`);
+}
+ 
+// while loop
+
+let myArray = ["Flash","Batman","superman"]
+
+let arr = 0
+while(arr < myArray.length){
+  // console.log(`value is ${myArray[arr]}`);
+  arr += 1
   
 }
 
+// do while
+
+let score = 12
+do {
+  console.log(`Score is ${score}`);
+  score++
+  
+} while(score <= 10);
---
+++ 05_iteration.js/02_order.js
@@ -0,0 +1,21 @@
+const arr = [1,2,3,4,5,6]
+
+// for(const iterator of object){
+
+// }
+
+for(const num of arr){
+  // console.log(num);
+  
+}
+
+const greetings = "hello world"
+
+for(const greet of greetings){
+  console.log(greet);
+  
+}
+
+// Maps
+
+const map = new Map()

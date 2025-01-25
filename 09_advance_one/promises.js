573d40bb8363eed09deae8041679bc04a0095091
Aditya More <144462086+AdityaMore007@users.noreply.github.com>
2025-01-25 22:55:30 +0530

    promises done

--- 09_advance_one/promises.js
+++ 09_advance_one/promises.js
@@ -30,3 +30,81 @@ promiseThree.then(function(user){
     console.log(user);
     
 })
+
+
+const promiseFour = new Promise(function(resolve,reject){
+    setTimeout(function(){
+        let error = false
+        if (!error){
+            resolve({username:"Aditya",password:"1223"})
+        }else{
+            reject('ERROR: Something went wrong!!')
+        }
+    },1000)
+})
+
+promiseFour.then((user)=>{
+    console.log(user);
+    return user.username
+}).then((username)=>{
+    console.log(username);
+    
+}).catch(function(error){
+    console.log(error);
+    
+}).finally(() => console.log("The promise is either resolved or rejected!!"))
+
+// Async and Await cannot handle the error.
+
+const promiseFive = new Promise ((resolve,reject) => {
+    setTimeout(function(){
+        let error = true
+        if (!error){
+            resolve({username:"Javascript",password:"1223"})
+        }else{
+            reject('ERROR: Something went wrong!!')
+        }
+    },1000)
+})
+
+// async function consumePromiseFive() {
+//     const response = await promiseFive
+//     console.log(response);
+// }
+
+// Above Code gives us error since Catch can reduce the error and ASYNC and AWAIT cannot handle those error!!.
+
+async function consumePromiseFive() {
+    try {
+        const response = await promiseFive
+        console.log(response);     
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+
+consumePromiseFive()
+
+// async function getAllUsers(){
+//     try {
+//         const response = await fetch('https://jsonplaceholder.typicode.com/users')
+//         const data = await response.json()
+//         console.log(data);
+//     } catch (error) {
+//         console.log(error)
+//     }
+// }
+
+// getAllUsers()
+
+
+fetch('https://api.github.com/users/AdityaMore007')
+.then((response) => {
+    return response.json()
+})
+.then((data) => {
+    console.log(data);
+    
+})
+.catch((error) => console.log(error))
